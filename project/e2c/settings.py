37331e75f8da7af390314e777ed28d44ab4a7e53
DESKTOP-477AOU5\tanukium <tanukium@gmail.com>
2020-11-11 18:03:34 +0900

    Deploy project to server.

--- project/e2c/settings.py
+++ project/e2c/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.179.225.250',
+                 'e2c.ninja']
 
 # Application definition
 
