defeda0cd01951199da6094a39b459daaa2152e4
Tanukium <tanukium@gmail.com>
2021-09-05 02:42:49 +0900

    Add pagination func & GitHub Markdown CSS.

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 import markdown
@@ -5,13 +6,39 @@ import markdown
 
 # Create your views here.
 
+def paginate_queryset(request, queryset, count):
+    """Pageオブジェクトを返す。
+
+    ページングしたい場合に利用してください。
+
+    countは、1ページに表示する件数です。
+    返却するPgaeオブジェクトは、以下のような感じで使えます。
+
+        {% if page_obj.has_previous %}
+          <a href="?page={{ page_obj.previous_page_number }}">Prev</a>
+        {% endif %}
+
+    また、page_obj.object_list で、count件数分の絞り込まれたquerysetが取得できます。
+
+    """
+    paginator = Paginator(queryset, count)
+    page = request.GET.get('page')
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    return page_obj
+
 
 def index(request):
     post = Post.objects.all().order_by('-created_time')
-    post = post[0:3]
+    page_obj = paginate_queryset(request, post, 6)
     return render(request, 'blog/index.html', {
         'title': 'Excel -> CSV直行便',
-        'post': post,
+        'post': page_obj.object_list,
+        'page_obj': page_obj,
     })
 
 
