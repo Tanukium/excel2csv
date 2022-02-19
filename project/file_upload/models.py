91df9d14a44803c0c439fda5ed36a4f35ad6898b
DESKTOP-477AOU5\tanukium <32299287+Tanukium@users.noreply.github.com>
2022-02-20 02:30:37 +0900

    Add size exceeding for upload files.

--- project/file_upload/models.py
+++ project/file_upload/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 import os
 from django.conf import settings
-
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 # Define user directory path
+
+
+def file_size(value):
+    limit = 524000
+    if value.size > limit:
+        raise ValidationError('File too large. Size should not exceed 500KiB.')
 
 
 def user_directory_path(instance, filename):
@@ -12,7 +18,7 @@ def user_directory_path(instance, filename):
 
 
 class File(models.Model):
-    file = models.FileField(upload_to='files', null=True)
+    file = models.FileField(upload_to='files', null=True, validators=[file_size])
 
     def abspath_file(self):
         root = settings.MEDIA_ROOT
