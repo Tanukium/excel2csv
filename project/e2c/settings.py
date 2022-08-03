9972f2e8a5fa3af740be86d922c79d397138f0ac
marine/tanukium <32299287+Tanukium@users.noreply.github.com>
2022-08-04 00:26:36 +0900

    Import django-s3-sqlite.

--- project/e2c/settings.py
+++ project/e2c/settings.py
@@ -26,13 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = [
-    'e2c.ninja',
-    'www.e2c.ninja'
-    # '127.0.0.1',
+    # 'e2c.ninja',
+    # 'www.e2c.ninja'
+    '127.0.0.1',
 ]
 
 # Application definition
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'file_upload.apps.FileUploadConfig',
     'gunicorn',
-    'captcha'
+    'captcha',
+    'django_s3_sqlite'
 ]
 
 MIDDLEWARE = [
@@ -83,10 +84,18 @@ WSGI_APPLICATION = 'e2c.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django_s3_sqlite",
+        "NAME": "db.sqlite3",
+        "BUCKET": "e2c-ninja-stg"
     }
 }
 
@@ -127,18 +136,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_SECONDS = 120
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
+# SECURE_HSTS_SECONDS = 120
