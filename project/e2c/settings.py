605356c3e18cbcd8faf9669c91ac7cfb48171752
marine/tanukium <32299287+Tanukium@users.noreply.github.com>
2022-08-04 21:24:15 +0900

    Add S3 static files supporting

--- project/e2c/settings.py
+++ project/e2c/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'file_upload.apps.FileUploadConfig',
     'gunicorn',
     'captcha',
-    'django_s3_sqlite'
+    'django_s3_sqlite',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -127,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
